4ea0dc12d384f7d6e555bc2f7b0ee45feefaf171
Zach <zkdoran@gmail.com>
2023-06-12 14:59:51 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
 
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Router basename="/react-app-currency">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <p>test</p>
+        <Link className="navbar-brand" to="/">Broke or Balling?</Link>
       </nav>
       <Switch>
         <Route path="/" exact component={Home} />
