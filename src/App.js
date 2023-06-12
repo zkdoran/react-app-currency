b1fd8dd6837f435ecc76504216eb603f49d5ec3d
Zach <zkdoran@gmail.com>
2023-06-12 16:40:14 -0400

    setting up app nav bar and switch

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
-
+import Converter from './Converter';
+import Worldlist from './Worldlist';
 
 const App = () => {
   return (
     <Router basename="/react-app-currency">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Broke or Balling?</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Start Over</Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/Converter/">You vs. The Destination</Link>
+            </li>
+            <li className="nav-item active">
+              <Link className="nav-link" to="/Worldlist/">You vs. The World</Link>
+            </li>
+          </ul>
+        </div>      
       </nav>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/Converter/" component={Converter} />
+        <Route path="/Worldlist/" component={Worldlist} />
       </Switch>
     </Router>
   );
