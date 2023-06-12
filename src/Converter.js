00add0fa81530fc85b5870e7aa106559766ac9eb
Zach <zkdoran@gmail.com>
2023-06-12 18:23:31 -0400

    added empty components

--- src/Converter.js
+++ src/Converter.js
@@ -1,2 +1,13 @@
 import { Link } from "react-router-dom";
 import { json, checkStatus } from './utils';
+
+class Converter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+}
+
+export default Converter;
