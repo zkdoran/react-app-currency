827585f5585eb51ec7086bfa7cf1ff34fa143e07
Zach <zkdoran@gmail.com>
2023-06-12 14:29:59 -0400

    import typo fix

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { json, checkStatus } from '.utils';
+import { json, checkStatus } from './utils';
 
 class StartOver extends React.Component {
   constructor(props) {
