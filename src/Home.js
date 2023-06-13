de0b4d2362ffe1bef61c8955afafb57aa04a81f3
Zach <zkdoran@gmail.com>
2023-06-13 14:37:34 -0400

    setting up home

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,39 @@ import React from 'react';
 import { json, checkStatus } from './utils';
 
 class StartOver extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencies: null,
+    };
+  }
+
+  componentDidMount () {   
+    fetch('https://api.frankfurter.app/currencies')
+    .then(checkStatus)
+    .then(json)
+    .then((data) => {
+      this.setState({ currencies: data });
+    })
+    .catch((error) => {
+      this.setState({ error: error.message });
+      console.log(error);
+    })
+  }
+
 
 
   render() {
     return (
-      <div>
-        <p>test</p>
+      <div className="row">
+        <div className="col">
+          <select>
+            <option>Option 1</option>
+          </select>
+        </div>
+        <div className="col">
+
+        </div>
       </div>
     )
   }
